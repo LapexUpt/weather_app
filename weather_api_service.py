e944b098ba18094d06e5ecf9540c78994287476a
lapex <layu0191@gmail.com>
2023-04-01 12:34:35 +0700

    Добился ответа 200 от API

--- weather_api_service.py
+++ weather_api_service.py
@@ -1,9 +1,8 @@
 import requests
 import coordinates as coord
 
-excl = "current" # current/minutely/hourly/daily/alerts
 opw_api_key = "90ecafcfe032346e5aadc13164335a8b"
-url = "https://api.openweathermap.org/data/3.0/onecall"
-params = f"?lat={coord.curr_lat}&lon={coord.curr_lat}&exclude={excl}&appid={opw_api_key}"
+url = "https://api.openweathermap.org/data/2.5/weather"
+params = f"?lat={coord.curr_lat}&lon={coord.curr_lon}&appid={opw_api_key}"
 req = requests.get(url + params)
-print(req)
+print(req.text)
